1397d2b0a64e992ed901420f97821130f1b3e75c
alexa <alexa@DESKTOP-2LNU9JA>
2022-06-08 23:18:37 -0500

    C

--- bases_exer/bases_exer/settings.py
+++ bases_exer/bases_exer/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'datoscr.apps.DatoscrConfig'
+    'datoscr.apps.DatoscrConfig',
 ]
 
 MIDDLEWARE = [
--- bases_exer/bases_exer/urls.py
+++ bases_exer/bases_exer/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
 from datoscr import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('books/', views.books, name='books'),
